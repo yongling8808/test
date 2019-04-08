f00521079880be2b228aad2effa4bc974c12c9e5
yongling <yongling@tencent.com>
2019-04-08 11:18:37 +0800

    test

---
+++ test/swofflineframework/serviceworker.js
@@ -0,0 +1,42 @@
+ const OFFLINE_CACHE = 'v5'; 
+   
+  
+this.addEventListener('install', function(event) {
+	
+	  var urlsToPrefetch = [
+    'index.html',
+    'snowTroopers.jpg',
+    'app.js',
+  	];
+
+event.waitUntil(
+    caches.open(OFFLINE_CACHE).then(function(cache) {
+      return cache.addAll(urlsToPrefetch);
+    })
+  );
+  
+          
+});
+         
+      
+this.addEventListener('activate', function(event) {
+});
+   
+ 
+this.addEventListener('fetch', function(event)  { 
+     
+   
+  console.log('serviceworker thread onfetch function');
+  console.log('this comes frome server!');
+  //console.log('Handling fetch event for '+ event.request.url);
+  event.respondWith(
+        caches.open(OFFLINE_CACHE).then(function(cache) {
+          //console.log('load from cache:'+ event.request.url);
+          return cache.match(event.request.url);
+        }).catch(function() {
+          console.log('match catch...');
+          return;
+        })
+    ); 
+  console.log('Handling fetch event end!'); 
+}); 
